ba0b7d0f6b8cf4079ceb4e41dbfb5346c00d1358
Dmitrii Kuragin <kuragin@chromium.org>
2023-04-03 19:24:05 -0700

    Add texture on a cuboid.

--- controls.js
+++ controls.js
@@ -2,24 +2,31 @@
 
 const kVertexShaderSource = `
   attribute vec4 aVertexPosition;
+  attribute vec2 aTextureCoord;
+
   attribute vec4 aVertexColor;
 
   uniform mat4 uModelViewMatrix;
   uniform mat4 uProjectionMatrix;
 
   varying lowp vec4 vColor;
+  varying highp vec2 vTextureCoord;
 
   void main() {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vTextureCoord = aTextureCoord;
     vColor = aVertexColor;
   }
 `;
 
 const kFragmentShaderSource =`
+  varying highp vec2 vTextureCoord;
   varying lowp vec4 vColor;
 
+  uniform sampler2D uSampler;
+
   void main() {
-    gl_FragColor = vColor;
+     gl_FragColor = vColor + texture2D(uSampler, vTextureCoord);
   }
 `;
 
@@ -30,13 +37,40 @@ function loadShader(gl, type, source) {
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    const error_message = `Failed to compile shader ${gl.getShaderInfoLog(shader)}`;
     gl.deleteShader(shader);
 
-    throw `Failed to compile shader ${gl.getShaderInfoLog(shader)}`
+    throw error_message;
   }
 
   return shader
 };
+
+function loadTexture(gl, url, callback) {
+  const level = 0;
+  const internal_format = gl.RGBA;
+  const src_format = gl.RGBA;
+  const type = gl.UNSIGNED_BYTE;
+
+  const image = new Image();
+  image.onload = () => {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internal_format, src_format, type, image);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    callback(texture);
+  };
+
+  image.onerror = () => {
+    callback(null);
+  };
+
+  image.src = url;
+}
 
 function createProgram(gl, vertex_shader_source, fragment_shader_source) {
   const vertex_shader = loadShader(gl, gl.VERTEX_SHADER, vertex_shader_source);
@@ -69,10 +103,13 @@ function createProgram(gl, vertex_shader_source, fragment_shader_source) {
     attrib_locations: {
       vertex_position: gl.getAttribLocation(program, "aVertexPosition"),
       vertex_color: gl.getAttribLocation(program, "aVertexColor"),
+      texture_coordinates: gl.getAttribLocation(program, "aTextureCoord"),
+
     },
     uniform_locations: {
       projection_matrix: gl.getUniformLocation(program, "uProjectionMatrix"),
       model_view_matrix: gl.getUniformLocation(program, "uModelViewMatrix"),
+      sampler: gl.getUniformLocation(program, "uSampler"),
     },
   };
 
@@ -93,13 +130,13 @@ function createProgram(gl, vertex_shader_source, fragment_shader_source) {
   const color_buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
 
-  const face_colors= [
-    [1.0, 1.0, 1.0, 1.0],
-    [1.0, 0.0, 0.0, 1.0],
-    [0.0, 1.0, 0.0, 1.0],
-    [0.0, 0.0, 1.0, 1.0],
-    [1.0, 1.0, 0.0, 1.0],
-    [1.0, 0.0, 1.0, 1.0],
+  const face_colors = [
+    [1.0, 1.0, 1.0, 0.5],
+    [1.0, 0.0, 0.0, 0.5],
+    [0.0, 1.0, 0.0, 0.5],
+    [0.0, 0.0, 1.0, 0.5],
+    [1.0, 1.0, 0.0, 0.5],
+    [1.0, 0.0, 1.0, 0.5],
   ];
 
   var colors = [];
@@ -155,19 +192,37 @@ function createProgram(gl, vertex_shader_source, fragment_shader_source) {
 
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
+  const texture_coordinates_buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texture_coordinates_buffer);
+  const texture_coordinates = [
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texture_coordinates), gl.STATIC_DRAW);
+
   const buffers = {
     position: position_buffer,
     color: color_buffer,
+    texture_coordinates: texture_coordinates_buffer,
     indices: index_buffer,
   };
 
-  function drawAnimationFrame(now) {
-    draw(gl, program_info, buffers);
-    requestAnimationFrame(drawAnimationFrame);
-  };
+  loadTexture(gl, "./smile.png", (texture) => {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 
-  requestAnimationFrame(drawAnimationFrame);
-  console.log("done");
+    function drawAnimationFrame(now) {
+      draw(gl, program_info, texture, buffers);
+      requestAnimationFrame(drawAnimationFrame);
+    };
+    requestAnimationFrame(drawAnimationFrame);
+    console.log("done");
+  });
+
 })();
 
 var rotation = {
@@ -178,7 +233,7 @@ var rotation = {
 
 var zoom = 6.0;
 
-function draw(gl, program_info, buffers){
+function draw(gl, program_info, texture, buffers){
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.enable(gl.DEPTH_TEST);
@@ -205,12 +260,18 @@ function draw(gl, program_info, buffers){
 
   setPositionAttribute(gl, buffers, program_info)
   setColorAttribute(gl, buffers, program_info)
+  setTextureAttribute(gl, buffers, program_info);
 
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
 
   gl.useProgram(program_info.program);
   gl.uniformMatrix4fv(program_info.uniform_locations.projection_matrix, false, projection_matrix);
   gl.uniformMatrix4fv(program_info.uniform_locations.model_view_matrix, false, model_view_matrix);
+
+  gl.activeTexture(gl.TEXTURE0);
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.uniform1i(program_info.uniform_locations.sampler, 0);
 
   {
     const offset = 0;
@@ -255,6 +316,25 @@ function setColorAttribute(gl, buffers, programInfo) {
   );
   gl.enableVertexAttribArray(programInfo.attrib_locations.vertex_color);
 };
+
+function setTextureAttribute(gl, buffers, programInfo) {
+  const num = 2;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.texture_coordinates);
+  gl.vertexAttribPointer(
+    programInfo.attrib_locations.texture_coordinates,
+    num,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+  gl.enableVertexAttribArray(programInfo.attrib_locations.texture_coordinates);
+}
+
 
 var rotation_is_active = false;
 var last_position = {
